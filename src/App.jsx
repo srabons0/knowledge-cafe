c82d04bcfd0dc9794f06a24ca5c68218227bd70f
srabons0 <srabontuno@gmail.com>
2024-02-27 12:37:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './component/header/Header'
+import Blog from './component/blogs/Blog'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+          <Header></Header>
+          <Blog></Blog>
+          
+           
+ 
+    </>
+  )
+}
+
+export default App
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-6xl text-center my-7 font-bold font-sans'>Knowledge Cafe</h1>
+
+             
+        </div>
+    );
+};
+
+export default Header;
